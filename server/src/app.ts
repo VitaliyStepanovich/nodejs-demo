a1809e7ce8b69a116f8cdc32cef06e8f576d5fac
VitaliyStepanovich <77295427+VitaliyStepanovich@users.noreply.github.com>
2021-07-22 18:36:09 +0300

    faet: process.env.PORT for express app

--- server/src/app.ts
+++ server/src/app.ts
@@ -23,4 +23,4 @@ app.use(/^(?!\/api\/)/, (req, res) => {
 app.use('/api/categories', categories);
 app.use('/api/items', items);
 
-app.listen(3000, () => console.log('Server started on http://localhost:3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Server started on http://localhost:3000'));
